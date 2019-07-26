82adcc97738e541997b16fc149048e2a8deeef73
mru24 <mru24@yahoo.com>
2019-07-26 16:43:12 +0100

    commit

--- index.php
+++ index.php
@@ -1,62 +1,52 @@
-<!-- code,description,width,depth,height,price -->
-
 <?php
-// Data from file 1
-$file_data = array();
 
-// Matches that are in both files
-$matches = array();
+$handle = fopen("april.csv", "r");
 
-// id,name,width,depth,height,price
-$handle = fopen("1.csv", "r");
-while (list($name, $quantity) = fgetcsv($handle, 1000))
+while (list($name, $quantity, $sku) = fgetcsv($handle, 1000))
 {
-  $file_1_names[] = $name;
-  $file_1_names_striped[] = strtolower(preg_replace('/\s/', '', $name));
-  $quantity_data[] = $quantity;
+  $file_1_name[] = $name;
+  $file_1_sku[] = $sku;
+  $file_1_sku_striped[] = strtolower(preg_replace('/\s/', '', $sku));
+  $file_1_quantity[] = $quantity;
+  $array1 = [
+    $file_1_name,
+    $file_1_sku,
+    $file_1_quantity
+  ];
 }
 fclose($handle);
 
 
-// strtolower(preg_replace('/\s/', '',
-
-$handle = fopen("2.csv", "r");
-while (list($name, $unused1, $unused2, $sku, $price) = fgetcsv($handle, 1000))
+$handle = fopen("catalog.csv", "r");
+while (list($sku, $name, $mg) = fgetcsv($handle, 1000))
 {
-  $file_2_names[] = $name;
-  $file_2_names_striped[] = strtolower(preg_replace('/\s/', '', $name));
-  $sku_data[] = $sku;
-
-  if(in_array(strtolower(preg_replace('/\s/', '', $name)), $file_1_names_striped))
+  // if(in_array(strtolower(preg_replace('/\s/', '', $sku)), $file_1_sku_striped))
+  if(in_array($sku, $file_1_sku))
   {
-    $names[] = $name;
-    $skus[] = $sku;
+    $file_2_sku[] = $sku;
+    $file_2_name[] = $name;
+    $file_2_mg[] = $mg;
   }
 }
 fclose($handle);
 
+$file = fopen("report.csv", "w") or die("Unable to open file!");
 
-foreach ($names as $key => $value) {
-  echo $skus[$key] . ' / ' . $value . ' / ' . $key . ' / ' . $quantity_data[$key] . "\n";
+$main_key =  count($file_2_sku) + 1;
+
+
+
+foreach ($file_2_sku as $key => $value) {
+  $data[] =  html_entity_decode($file_2_name[$key] . '/-' . $file_2_mg[$key] . '/-' . $file_2_sku[$key]);
 }
 
-// print_r($skus);
-// print_r($names);
-// print_r($quantities);
-// echo '<br> ************* <br>';
+print_r($data);
 
-// foreach ($skus as $key => $sku) {
-//   echo $sku . ' / ' . $names[$key] . ' / ' . "\n";
-// }
+foreach ( $data as $line ) {
+    $val = explode("/-", $line);
+    fputcsv($file, $val);
+}
 
-
-// foreach ($prices as $key => $price) {
-//   $price = $price + 100;
-//   echo strtoupper($ids[$key]) . ' - ' . $price . '<br>';
-// }
-
-// foreach ($matches as $key => $match) {
-//   echo strtoupper($match) . ' - ' . $prices[$key] . '<br>';
-// }
+fclose($file);
 
 ?>
